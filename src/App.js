9f650f5438a442564623622fc5e7b0bf82bfa38d
jackkitte <jackkitte735@gmail.com>
2019-08-16 15:17:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import PageOne from './components/PageOne';
+import PageTwo from './components/PageTwo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Route path="/" component={PageOne} exact={true} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </div>
+    )
+  }
 }
 
 export default App;
